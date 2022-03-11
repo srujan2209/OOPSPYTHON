6782a9fb04430875b89291231292a03522f011ea
Srujan <srujankumar1996@gmail.com>
2022-03-11 12:56:20 +0530

    class and defining it's attributes

---
+++ code.py
@@ -0,0 +1,12 @@
+# Creating a Class and Defining it's Attributes Teddy Bear Mold
+class Teddy:
+    quantity = 200  # This is the property of a class.
+
+# This is the Object
+teddy1 = Teddy()
+# This is the attribute of a particular object
+print(teddy1.quantity)
+teddy2 = Teddy()
+print(teddy2.quantity)
+
+# Suppose if we want to make one teddy color as red, and another teddy color as white in the case we will use the instance attributes.
